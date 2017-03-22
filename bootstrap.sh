2b7fd358f8ebc8442610c6b2ca06a290f17d77e1
Daniel C <dchan_dev@outlook.de>
2017-03-22 22:38:39 +0100

    fixed bootstrap sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-Update () {
-    echo "-- Update packages --"
-    sudo apt-get update
-    sudo apt-get upgrade
-}
-Update
 sudo add-apt-repository ppa:ondrej/php
-sudo apt-get install php7.1
-sudo apt-get install php-xdebug
-Update
-sudo apt-get install php7.1-mysql libapache2-mod-php7.1
+echo "-- Update packages --"
+sudo apt-get update
+sudo apt-get -y install php7.1 php-xdebug php7.1-mysql libapache2-mod-php7.1 php7.1-mbstring
+echo "-- upgrade box --"
+sudo apt-get update
+sudo apt-get -y upgrade
+echo "-- switch php --"
 sudo a2dismod php5
-sudo a2enmod php
-echo "zend_extension=xdebug.so" >> /etc/php/apache2/conf.d/20-xdebug.ini &&\
-echo "xdebug.remote_enable=1" >> /etc/php/apache2/conf.d/20-xdebug.ini &&\
-echo "xdebug.remote_connect_back= 1">> /etc/php/apache2/conf.d/20-xdebug.ini &&\
-echo "xdebug.remote_port=9000" >> /etc/php/apache2/conf.d/20-xdebug.ini &&\
-echo "xdebug.remote_host=192.168.33.10" >> /etc/php/apache2/conf.d/20-xdebug.ini &&\
+sudo a2enmod php7.1
+echo "-- config xdebug --"
+echo "zend_extension=xdebug.so" > /etc/php/7.1/apache2/conf.d/20-xdebug.ini &&\
+echo "xdebug.remote_enable=1" >> /etc/php/7.1/apache2/conf.d/20-xdebug.ini &&\
+echo "xdebug.remote_connect_back= 1">> /etc/php/7.1/apache2/conf.d/20-xdebug.ini &&\
+echo "xdebug.remote_port=9000" >> /etc/php/7.1/apache2/conf.d/20-xdebug.ini &&\
+echo "xdebug.remote_host=192.168.33.10" >> /etc/php/7.1/apache2/conf.d/20-xdebug.ini &&\
+echo "-- restart apache2 --"
 sudo apachectl restart
+sudo sudo service apache2 restart
